437102e9bd1f9540cfea13a871d99cb2c29fe65f
sthompson-athenian <sthompson@athenian.org>
2025-11-10 23:34:17 -0800

    Add subsetSum + knapsack problem outlines, and tests

---
+++ src/main/java/SubsetSum.java
@@ -0,0 +1,19 @@
+public class SubsetSum {
+    // Given an array of integers and an int sum, determine if some subset
+    // of those integers adds up to exactly sum
+
+    // For example, if our input array is [1, 1, 4], the possible sums which would
+    // return true are 0, 1, 2, 4, 5, 6. Any other sum value would return false.
+    public static boolean subsetSum(int[] arr, int sum) {
+        return false;
+    }
+
+    // Given some items with both weight and value, as well as a max capacity
+    // determine the most value we can fit in the knapsack
+
+    // Note that the weight and value of each item is stored in same index of the relevant array
+    // (e.g. weights[2] is the weight of item 2 and values[2] is that same item's value)
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        return -1;
+    }
+}
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -3,35 +3,75 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTests {
+
+    // --------------------------
+    // Test 1: Basic Subset Sum
+    // --------------------------
     @Test
-    public void testChangeNoMemo() {
-        CoinChange c1 = new CoinChange();
-        assertEquals(4, c1.changeNoMemo(new int[] {1, 4, 5}, 18));
-        c1 = new CoinChange();
-        assertEquals(7, c1.changeNoMemo(new int[] {1, 4, 5}, 33));
-        c1 = new CoinChange();
-        assertEquals(4, c1.changeNoMemo(new int[] {1, 3, 5}, 16));
-        c1 = new CoinChange();
-        assertEquals(7, c1.changeNoMemo(new int[] {1, 3, 5}, 33));
+    public void testBasicSubsetSum() {
+        int[] arr1 = {1, 1, 3, 7};
+
+        // Does {1, 1, 3, 7} have a subset sum of 5?
+        assertTrue(SubsetSum.subsetSum(arr1, 5));
+
+        // Does {1, 1, 3, 7} have a subset sum of 11?
+        assertTrue(SubsetSum.subsetSum(arr1, 11));
+
+        // Does {1, 1, 3, 7} have a subset sum of 12?
+        assertTrue(SubsetSum.subsetSum(arr1, 12));
+
+        // Does {1, 1, 3, 7} have a subset sum of 6?
+        assertFalse(SubsetSum.subsetSum(arr1, 6));
     }
 
+    // --------------------------
+    // Test 2: More Subset Sum
+    // --------------------------
     @Test
-    public void testChangeMemoSmall() {
-        CoinChange c1 = new CoinChange();
-        assertEquals(4, c1.changeWithMemo(new int[] {1, 4, 5}, 18));
-        c1 = new CoinChange();
-        assertEquals(7, c1.changeWithMemo(new int[] {1, 4, 5}, 33));
-        c1 = new CoinChange();
-        assertEquals(4, c1.changeWithMemo(new int[] {1, 3, 5}, 16));
-        c1 = new CoinChange();
-        assertEquals(7, c1.changeWithMemo(new int[] {1, 3, 5}, 33));
+    public void testMoreSubsetSum() {
+        int[] arr2 = {1, 1, 5, 6, 8, 10, 12, 14, 16};
+
+        // Does {1, 1, 5, 6, 8, 10, 12, 14, 16} have a subset sum of 11?
+        assertTrue(SubsetSum.subsetSum(arr2, 11));
+
+        // Does {1, 1, 5, 6, 8, 10, 12, 14, 16} have a subset sum of 28?
+        assertTrue(SubsetSum.subsetSum(arr2, 28));
+
+        // Does {1, 1, 5, 6, 8, 10, 12, 14, 16} have a subset sum of 32?
+        assertTrue(SubsetSum.subsetSum(arr2, 32));
+
+        // Does {1, 1, 5, 6, 8, 10, 12, 14, 16} have a subset sum of 73?
+        assertTrue(SubsetSum.subsetSum(arr2, 73));
     }
 
+    // --------------------------
+    // Test 3: Basic Knapsack
+    // --------------------------
     @Test
-    public void testChangeMemoLarge() {
-        CoinChange c1 = new CoinChange();
-        assertEquals(118, c1.changeWithMemo(new int[] {1, 4, 5}, 588));
-        c1 = new CoinChange();
-        assertEquals(258, c1.changeWithMemo(new int[] {1, 4, 5}, 1288));
+    public void testBasicKnapsack() {
+        // Test case 1
+        int[] weights1 = {5, 10, 25};
+        int[] values1 = {70, 90, 140};
+        assertEquals(160, SubsetSum.knapsack(weights1, values1, 25));
+
+        // Test case 2
+        int[] weights2 = {5, 10, 20};
+        int[] values2 = {150, 60, 140};
+        assertEquals(290, SubsetSum.knapsack(weights2, values2, 30));
+
+        // Test case 3
+        int[] weights3 = {5, 20, 10};
+        int[] values3 = {50, 140, 60};
+        assertEquals(200, SubsetSum.knapsack(weights3, values3, 30));
+    }
+
+    // --------------------------
+    // Test 4: Advanced Knapsack
+    // --------------------------
+    @Test
+    public void testAdvancedKnapsack() {
+        int[] weights4 = {85, 26, 48, 21, 22, 95, 43, 45, 55, 52};
+        int[] values4 = {79, 32, 47, 18, 26, 85, 33, 40, 45, 59};
+        assertEquals(117, SubsetSum.knapsack(weights4, values4, 101));
     }
 }
